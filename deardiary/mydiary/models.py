2202a93cc3704c29f69b6fe9d15b4e86c7582861
akash <ryderaka@gmail.com>
2018-03-09 18:46:11 +0530

    added form

--- deardiary/mydiary/models.py
+++ deardiary/mydiary/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import os
+from django.utils import timezone
+from PIL import Image
+from resizeimage import resizeimage
 
-# Create your models here.
+
+def profile_path_and_rename(instance, filename):
+    upload_to = 'static/profile/'+str(instance.phone)
+    return os.path.join(upload_to, filename)
+
+def image_resize(image, image_path):
+    image = Image.open(image)
+    if image.size[0] > 400:  # if width is greater than 400
+        new_image = resizeimage.resize_width(image, 400)
+        new_image.save(image_path)
+
+    elif image.size[1] > 400:  # if height is greater than 400
+        new_image = resizeimage.resize_height(image, 400)
+        new_image.save(image_path)
+
+
+class TodoUser(models.Model):
+    phone = models.BigIntegerField(blank=True, unique=True)
+    name = models.CharField(max_length=60, blank=True, unique=False)
+    email = models.EmailField(unique=False, blank=True, null=True)
+    image = models.ImageField(upload_to=profile_path_and_rename, blank=True, null=True)
+    time = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return str(self.phone)
+
+    def save(self, *args, **kwargs):
+        super(TodoUser, self).save()
+        if self.image not in [None, '']:
+            image_resize(self.image, self.image.path)
